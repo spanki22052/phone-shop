30744d833d34d648895c56c57a4785dd9c763934
spanki22052 <useful22052@gmail.com>
2021-06-08 10:25:32 +0300

    some changes

--- services/src/index.ts
+++ services/src/index.ts
@@ -1,1 +1,17 @@
-console.log('hello');
+import mongoose from 'mongoose';
+import express from 'express';
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: `${__dirname}/../.env` });
+
+const app = express();
+const port: number = 3000;
+
+mongoose.connect(process.env.DB_URL as string, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.listen(port, (): void => {
+  console.log(`Working on port ${port}`);
+});
