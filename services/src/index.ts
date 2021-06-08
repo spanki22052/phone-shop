f8024ab6edddaac1000903a2e3bab0eabd98bdc6
spanki22052 <useful22052@gmail.com>
2021-06-08 15:01:38 +0300

    graphql ready

--- services/src/index.ts
+++ services/src/index.ts
@@ -1,28 +1,50 @@
 import mongoose from 'mongoose';
-import express from 'express';
-import * as dotenv from 'dotenv';
+import express, { Application } from 'express';
 import Phone from './models/phone';
+import { graphql, buildSchema } from 'graphql';
+import { ApolloServer, gql } from 'apollo-server-express';
+import * as dotenv from 'dotenv';
 
 dotenv.config({ path: `${__dirname}/../.env` });
 
-const app = express();
+const app: Application = express();
 const port: number = 3000;
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+
+const server: ApolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+  playground: true,
+});
+
+server.applyMiddleware({ app });
 
 mongoose.connect(process.env.DB_URL as string, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-const newPhone = new Phone({
-  title: 'Iphone 7 Plus 256gb',
-  description: 'Good phone.',
-  photos: [],
-  price: 25000,
-  brand: 'Iphone',
-});
+// const newPhone = new Phone({
+//   title: 'Iphone 7 Plus 256gb',
+//   description: 'Good phone.',
+//   photos: [],
+//   price: 25000,
+//   brand: 'Iphone',
+// });
 
-newPhone.save();
+// newPhone.save();
 
 app.listen(port, (): void => {
-  console.log(`Working on port ${port}`);
+  console.log(`Working on port ${port} - ${server.graphqlPath}`);
 });
